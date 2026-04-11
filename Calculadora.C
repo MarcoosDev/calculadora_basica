94963aa4a1975b628da4f961d24642e3800b41b4
MarcosDevphb <marcosdevphb@gmail.com>
2026-04-10 21:34:53 -0300

    Create Calculadora.C

---
+++ Calculadora.C
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float menu(int m, float a, float b, int *erro);
+int metodo_de_calculo();
+float numero();
+float segundo_numero();
+
+
+int main() {
+    float pri, seg, resultado;
+    int m, erro = 0;
+
+    pri = numero();
+    seg = numero();
+    m = metodo_de_calculo();
+
+    resultado = menu(m, pri, seg, &erro);
+
+    while (erro == 1) {
+        m = metodo_de_calculo();
+        resultado = menu(m, pri, seg, &erro);
+    }
+    if (erro == 2) {
+        printf("Resultado: Erro de calculo, divisoes por 0 sao matematicamente impossiveis");
+        return 0;
+    }
+
+    printf("Resultado: %.2f", resultado);
+    return 0;
+}
+
+
+float menu(int m, float a, float b, int *erro) {
+    float r = 0;
+    *erro = 0;
+
+    switch (m) {
+        case 1: r = a + b; break;
+        case 2: r = a - b; break;
+        case 3:
+            if (b == 0) {
+                printf("Erro de calculo, fator b = 0.\n");
+                *erro = 2; break;
+            }
+            else {
+                r = a / b;
+                break;}
+        case 4: r = a * b; break;
+        default:
+            printf("opcao invalida\n");
+            *erro = 1;
+    }
+    return r;
+}
+
+
+int metodo_de_calculo() {
+    int res;
+    printf("qual o metodo?\n1-Soma\n2-Subtracao\n3-Divisao\n4-Multiplicacao\n");
+    scanf("%d", &res);
+    return res;
+};
+
+float numero() {
+    float num;
+    printf("me diga um numero\n");
+    scanf("%f", &num);
+    return num;
+};
