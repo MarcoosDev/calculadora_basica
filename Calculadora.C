480c314b1b41ea9e85d06571a25505323f311bc2
Marcos Vinicius <marcosdevphb@gmail.com>
2026-04-13 23:04:47 -0300

    Melhoria Calculadora.C

--- Calculadora.C
+++ Calculadora.C
@@ -3,16 +3,15 @@
 
 float menu(int m, float a, float b, int *erro);
 int metodo_de_calculo();
-float numero();
-float segundo_numero();
-
+float ler_numero();
+float dividir(int *erro, float a, float b);
 
 int main() {
     float pri, seg, resultado;
     int m, erro = 0;
 
-    pri = numero();
-    seg = numero();
+    pri = ler_numero();
+    seg = ler_numero();
     m = metodo_de_calculo();
 
     resultado = menu(m, pri, seg, &erro);
@@ -22,14 +21,22 @@ int main() {
         resultado = menu(m, pri, seg, &erro);
     }
     if (erro == 2) {
-        printf("Resultado: Erro de calculo, divisoes por 0 sao matematicamente impossiveis");
+        printf("Resultado: Erro de calculo, divisoes por 0 sao matematicamente impossiveis.");
         return 0;
     }
 
     printf("Resultado: %.2f", resultado);
     return 0;
-}
+};
 
+float dividir(int *erro, float a, float b){
+    if (b == 0) {
+        printf("Erro de calculo, fator b = 0.\n");
+        *erro = 2;
+        return 0;
+    }
+    return a / b;
+}
 
 float menu(int m, float a, float b, int *erro) {
     float r = 0;
@@ -38,22 +45,14 @@ float menu(int m, float a, float b, int *erro) {
     switch (m) {
         case 1: r = a + b; break;
         case 2: r = a - b; break;
-        case 3:
-            if (b == 0) {
-                printf("Erro de calculo, fator b = 0.\n");
-                *erro = 2; break;
-            }
-            else {
-                r = a / b;
-                break;}
+        case 3: r = dividir(erro, a, b); break;
         case 4: r = a * b; break;
         default:
             printf("opcao invalida\n");
             *erro = 1;
     }
     return r;
-}
-
+};
 
 int metodo_de_calculo() {
     int res;
@@ -62,7 +61,7 @@ int metodo_de_calculo() {
     return res;
 };
 
-float numero() {
+float ler_numero() {
     float num;
     printf("me diga um numero\n");
     scanf("%f", &num);
